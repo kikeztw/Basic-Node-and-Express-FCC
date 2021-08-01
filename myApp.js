9ab1cb4ad7a42b2972a0c5c631b2f4b90c2a9d29
kikeztw <kikeztw@gmail.com>
2021-08-01 13:15:33 -0400

    Implement a Root-Level Request Logger Middleware, Chain Middleware to Create a Time Server, Get Route Parameter Input from the Client, Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var app = express();
 
+
+
+app.use(function(req, res, next){
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
 app.use('/public', express.static(__dirname + "/public"));
 
+
+app.route('/name')
+  .get(function(req, res){
+    const { query } = req;
+    res.json({ name: `${query.first} ${query.last}` })
+  })
+  
+
+app.get('/:word/echo', function(req, res){
+  const { params } = req;
+  res.send({ echo: params.word });
+});
+
+app.get('/now', 
+  function(req, res, next){
+    req.time = new Date().toDateString();
+    next();
+  },
+  function(req, res, next){ 
+    res.send({ time: req.time });    
+  }
+);
 
 app.get('/json', function(req, res){
   const isUpperCase = process.env.MESSAGE_STYLE === 'uppercase';
