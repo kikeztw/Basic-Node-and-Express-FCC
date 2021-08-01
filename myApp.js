57b1cb1d44c968b4732282bc966278bc943ca307
kikeztw <kikeztw@gmail.com>
2021-08-01 12:21:53 -0400

    added file send and json send

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use('/public', express.static(__dirname + "/public"));
+
+
+app.get('/json', function(req, res){
+  const isUpperCase = process.env.MESSAGE_STYLE === 'uppercase';
+  const message = 'Hello json';
+  res.json({ message:  isUpperCase ? message.toUpperCase() : message })
+});
 
 app.get('/file', function(req, res){
   const fileDir = __dirname + '/views/index.html';
