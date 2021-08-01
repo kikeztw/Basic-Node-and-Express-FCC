a48e1cc007eeb0db3da0993ee5787c0b1c355b5f
kikeztw <kikeztw@gmail.com>
2021-08-01 11:51:42 -0400

    added file send

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 
 
+app.get('/file', function(req, res){
+  const fileDir = __dirname + '/views/index.html';
+  res.sendFile(fileDir);
+});
+
 app.get('/', function(req, res){
   res.send('Hello Express');
 });
